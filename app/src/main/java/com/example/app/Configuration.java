f8dab97c885b6ded57533f473e98eadf42f57771
Piotr Kazmierczak <kazmierczak.piotr3@gmail.com>
2019-12-02 16:37:30 +0100

    Add basic @Configuration bean

---
+++ app/src/main/java/com/example/app/Configuration.java
@@ -0,0 +1,6 @@
+package com.example.app;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+}
---
+++ app/src/test/java/com/example/app/AppApplicationTest.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class AppApplicationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    void contextLoads() {
+
+        assert null != context.getBean(Configuration.class);
+    }
+}
