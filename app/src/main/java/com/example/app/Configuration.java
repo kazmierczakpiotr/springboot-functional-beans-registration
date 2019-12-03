2e59e4c58704a6a64b75f7c622772561797e3555
Piotr Kazmierczak <kazmierczak.piotr3@gmail.com>
2019-12-03 09:40:34 +0100

    Configuration implements ApplicationContextInitializer

--- app/src/main/java/com/example/app/Configuration.java
+++ app/src/main/java/com/example/app/Configuration.java
@@ -1,6 +1,13 @@
 package com.example.app;
 
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
+@org.springframework.context.annotation.Configuration
+public class Configuration implements ApplicationContextInitializer<GenericApplicationContext> {
+
+
+    @Override
+    public void initialize(GenericApplicationContext context) {
+    }
 }
